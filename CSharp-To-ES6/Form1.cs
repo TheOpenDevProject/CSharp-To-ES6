016c66812e22bd6d6bec94e915d94089cf7e26ac
TheOpenDevProject <tkgs28@gmail.com>
2016-04-14 17:30:27 +1000

    Updating regex

--- CSharp-To-ES6/Form1.cs
+++ CSharp-To-ES6/Form1.cs
@@ -20,7 +20,12 @@ namespace CSharptoJson
 
         private void button1_Click(object sender, EventArgs e)
         {
+            var x = Regex.Matches(this.textBox1.Text, "(?:public\\s|private\\s|protected\\s)\\s*(?:readonly\\s+)?(?<type>\\w+)\\s+(?<name>\\w+)");
             string input = this.textBox1.Text;
+            string finalClass = $"export class {Regex.Match(input, "(?<=public class )(.*)(?=)").ToString()}";
+            finalClass += "{" + Environment.NewLine;
+            finalClass += "constructor(){}" + Environment.NewLine; ;
+            finalClass += "toJson(){" + Environment.NewLine; ;
             string[] inputLines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
             string newList = "{";
             foreach (string currentLine in inputLines)
@@ -29,7 +34,9 @@ namespace CSharptoJson
                 newList += $"{identifier} : this.{identifier},";
             }
             newList += "}";
-            this.textBox2.Text = newList;
+            finalClass += "}";
+            this.textBox2.Text = finalClass;
+
         }
     }
 }
