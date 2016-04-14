94a700ae9dfe626f5059598a585e5d4328299948
TheOpenDevProject <tkgs28@gmail.com>
2016-04-14 16:56:21 +1000

    WIP: currently converts constructor properties to a JSON object

--- CSharptoJson/Form1.cs
+++ CSharptoJson/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,20 @@ namespace CSharptoJson
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = this.textBox1.Text;
+            string[] inputLines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            string newList = "{";
+            foreach (string currentLine in inputLines)
+            {
+                string identifier = Regex.Match(currentLine, "(?<=this.)(.*)(?=;)").ToString();
+                newList += $"{identifier} : this.{identifier},";
+            }
+            newList += "}";
+            this.textBox2.Text = newList;
         }
     }
 }
